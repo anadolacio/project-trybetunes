a9a786f01915d270bbf70e2324f6529140e4327a
anadolacio <anadolacio@hotmail.com>
2022-12-14 09:38:14 -0300

    resolução requisito 1

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Favorites extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-favorites"> oi</div>
+
+    );
+  }
+}
+
+export default Favorites;
