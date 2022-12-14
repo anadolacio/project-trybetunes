d963d94c5f0017174c1521890dd44bb0c5d622a7
anadolacio <anadolacio@hotmail.com>
2022-12-14 17:32:02 -0300

    resolução requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { createUser } from '../services/userAPI';
 import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
   state = {
